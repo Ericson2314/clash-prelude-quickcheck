8d8f21fc86bdbd41ca5d004b8e6fd17f34fa9939
John Ericson <Ericson2314@Yahoo.com>
2014-12-02 01:26:52 -0500

    Fix the BitVector instance

--- BitInstances.hs
+++ BitInstances.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Engn1630.Util.BitInstances where
+
+import           Prelude            hiding (sequence)
 
 import           Data.Bits
 
 import qualified Test.QuickCheck
 import           Test.QuickCheck
 
-import           CLaSH.Prelude      hiding (lift)
+import           CLaSH.Prelude      hiding (sequence, lift)
 import           CLaSH.Sized.Vector
 import           CLaSH.Promoted.Nat
 
@@ -21,10 +27,14 @@ iso2 = \case 0 -> False
              _ -> error"impossible"
 
 instance KnownNat n => Arbitrary (BitVector n) where
-  arbitrary = pack <$> (V.repeat <$> iso1 <$> arbitrary :: Gen (Vec n Bit))
+  arbitrary = fmap pack $ sequence $ V.repeat $ iso1 <$> arbitrary
 --  shrink l  = fmap iso1 $ shrink $ iso2 l
 
 
 inv :: Bits a => a -> a
 inv = complement
 
+sequence :: Applicative m => Vec n (m a) -> m (Vec n a)
+sequence = \case
+  Nil     -> pure Nil
+  m :> ms -> (:>) <$> m <*> sequence ms
