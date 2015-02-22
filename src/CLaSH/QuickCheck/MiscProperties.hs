cbb843f34f73d87f35b1a1d7601f9f5476395c2d
John Ericson <Ericson2314@Yahoo.com>
2015-02-22 16:27:28 -0500

    Remove abandoned modules for now

--- src/CLaSH/QuickCheck/MiscProperties.hs
+++
@@ -1,7 +0,0 @@
-module CLaSH.QuickCheck.MiscProperties where
-
-commutivity :: Eq a => (a -> a -> a) -> (a -> a -> Bool)
-commutivity f = \a b -> f a b == f b a
-
-associativity :: Eq a => (a -> a -> a) -> (a -> a -> a -> Bool)
-associativity f = \a b c -> f a (f b c) == f (f a b) c
